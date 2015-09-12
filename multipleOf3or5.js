5d545c2740e90541aa41f620ceb07ab84d5b5486
Rashard Shannon <shannon.rashard@yahoo.com>
2015-09-12 03:33:02 -0400

    first commit

---
+++ multipleOf3or5.js
@@ -0,0 +1,14 @@
+var count = 0;
+var multiples = 0;
+var sumOfMultiples = 0;
+
+while(count < 1000){
+    if(multiples % 3 === 0 || multiples % 5 === 0){
+        sumOfMultiples += multiples;
+    } else{
+    }
+    count++;
+    multiples++;
+}
+console.log(sumOfMultiples);
+# odin.js.project_euler 
